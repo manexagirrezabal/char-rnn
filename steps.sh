337d7a0d554a143a26c4c0bd27390e3131d7ca10
manexagirrezabal <manex.agirrezabal@gmail.com>
2016-02-05 17:02:48 +0100

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -7,3 +7,5 @@ th train.lua -seq_length 27 -batch_size 10 -print_every 40 -eval_val_every 100
 
 th train.lua -print_every 40 -eval_val_every 100 -losslog logs/dpbx2-default.txt
 th train.lua -seq_length 27 -batch_size 10 -print_every 40 -eval_val_every 100 -losslog logs/dpbx2-seq_len27-batch_size10.txt
+python plotloss.py logs/dpbx2-seq_len27-batch_size10.txt
+python callSampleMod.py cv/lm_lstm_epoch17.48_1.5583.t7
